4cf92bef54cdd754bc1b4dfed1ce5fb71370bff4
githubkabir123 <dehan2500@gmail.com>
2025-06-21 01:07:35 +0600

    alow crose to frontend req in backend

--- api/index.js
+++ api/index.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://192.168.0.199:5173', // allow only your frontend during development
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
